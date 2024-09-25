af67a0f2640bb4ab66d304c7229e4e0d0e312a51
moeez5251 <moeez66656@gmail.com>
2024-09-25 20:23:10 +0500

    Updating

---
+++ netlify/functions/getimages.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const path = require("path");
+
+exports.handler = async (event, context) => {
+  try {
+    const imagesDir = path.resolve(__dirname, "../../assets/images/");
+    let images = fs.readdirSync(imagesDir);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(images),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Unable to read images directory" }),
+    };
+  }
+};
---
+++ netlify/functions/slug.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const path = require("path");
+
+exports.handler = async (event, context) => {
+  try {
+    const { slug } = event.pathParameters;
+    const imagesDir = path.resolve(__dirname, `../../assets/images/${slug}`);
+    let images = fs.readdirSync(imagesDir);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(images),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: "Unable to Find that color",
+    };
+  }
+};
