662207074dea3c5733264f41ece186e5319aa094
moeez5251 <moeez66656@gmail.com>
2024-09-26 15:31:31 +0500

    Updating

--- netlify/functions/getimages.js
+++ netlify/functions/getimages.js
@@ -1,27 +1,17 @@
-// functions/listAllImages.js
-
 const fs = require("fs");
 const path = require("path");
-
 exports.handler = async (event, context) => {
   try {
-    const imagesDir = path.join(process.cwd(), "assets/images/");
-
-    if (!fs.existsSync(imagesDir)) {
-      throw new Error("Images directory not found");
-    }
-
-    const images = fs.readdirSync(imagesDir);
-    
+    const imagesDir = path.resolve(__dirname, "../../assets/images/");
+    let images = fs.readdirSync(imagesDir);
     return {
       statusCode: 200,
       body: JSON.stringify(images),
     };
   } catch (error) {
-    console.error("Error reading images directory:", error.message);
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: "Unable to read images directory", details: error.message }),
+      body: JSON.stringify({ error: "Unable to read images directory" }),
     };
   }
 };
--- netlify/functions/slug.js
+++ netlify/functions/slug.js
@@ -1,41 +1,18 @@
-// functions/listImagesBySlug.js
-
 const fs = require("fs");
 const path = require("path");
-
 exports.handler = async (event, context) => {
   try {
-    const imagesDir = path.join(process.cwd(), "assets/images/");
-    
-    const slug = event.path.split("/").pop(); 
-
-    if (!slug || slug.trim() === "") {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ error: "Slug is required" }),
-      };
-    }
-
-    const specificImagesDir = path.join(imagesDir, slug);
-    
-    if (!fs.existsSync(specificImagesDir)) {
-      return {
-        statusCode: 404,
-        body: JSON.stringify({ error: "Directory not found for the specified slug" }),
-      };
-    }
-
-    const images = fs.readdirSync(specificImagesDir);
-    
+    const { slug } = event.pathParameters;
+    const imagesDir = path.resolve(__dirname, `../../assets/images/${slug}`);
+    let images = fs.readdirSync(imagesDir);
     return {
       statusCode: 200,
       body: JSON.stringify(images),
     };
   } catch (error) {
-    console.error("Error reading images directory:", error.message);
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: "Unable to read images directory", details: error.message }),
+      body: "Unable to Find that color",
     };
   }
 };
