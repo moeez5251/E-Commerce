65fe397b9c1e7645ba32186463bd1fc482dc9fbf
moeez5251 <moeez66656@gmail.com>
2024-09-27 21:16:24 +0500

    Make Responsive and Fix some issues ✨⭐

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
-(async function main() {
+async function main() {
     let a = await fetch("https://stridestyle.netlify.app/.netlify/functions/getimages");
     let b = await a.json();
-    document.querySelector(".products").innerHTML="";
+    document.querySelector(".products").innerHTML = "";
     for (const element of b) {
         let fet = await fetch(`https://stridestyle.netlify.app/.netlify/functions/getslug?slug=${element}`)
         let response = await fet.json();
@@ -51,24 +51,24 @@
                     document.querySelectorAll(".item").forEach(a => {
                         a.querySelector(".plus").addEventListener("click", () => {
                             a.querySelector(".val").innerHTML++;
-                            a.querySelector(".Prices").innerHTML = a.querySelector(".val").innerHTML*e.querySelector(".Price").innerHTML.split("$")[0]+" $";
+                            a.querySelector(".Prices").innerHTML = a.querySelector(".val").innerHTML * e.querySelector(".Price").innerHTML.split("$")[0] + " $";
 
                         })
 
                         a.querySelector(".min").addEventListener("click", () => {
-                            
-                            if (a.querySelector(".val").innerHTML <1) {
+
+                            if (a.querySelector(".val").innerHTML < 1) {
                                 console.log(a);
                                 a.querySelector(".min").classList.add("disabled");
                                 if (confirm("Do You Want to Delete Item !")) {
                                     a.remove();
                                     document.querySelector(".ab").innerHTML--;
-                                    
+
                                 }
                                 return;
                             }
                             a.querySelector(".val").innerHTML--;
-                            a.querySelector(".Prices").innerHTML = a.querySelector(".val").innerHTML*e.querySelector(".Price").innerHTML.split("$")[0]+" $";
+                            a.querySelector(".Prices").innerHTML = a.querySelector(".val").innerHTML * e.querySelector(".Price").innerHTML.split("$")[0] + " $";
                         })
 
                     })
@@ -85,23 +85,29 @@
 
     }
 
-})();
+}
+main();
 document.querySelector(".relative span").addEventListener("click", () => {
     document.querySelector(".cart").style.display = "flex";
 })
-document.querySelector(".inp").addEventListener("input",(e)=>{
-    document.querySelector(".products").innerHTML=`<div class="loader"></div>`
+document.querySelector(".inp").addEventListener("input", (e) => {
+    if (e.target.value.trim() == "") {
+        document.querySelector(".products").innerHTML = "";
+        main();
+        return;
+    }
+    document.querySelector(".products").innerHTML = `<div class="loader"></div>`
     fetch(`https://stridestyle.netlify.app/.netlify/functions/getslug?slug=${e.target.value}`)
-    .then(response => response.json())
-    .then(response => {
-    document.querySelector(".products").innerHTML="";
+        .then(response => response.json())
+        .then(response => {
+            document.querySelector(".products").innerHTML = "";
 
-        console.log(e.target.value, response);
-        document.querySelector(".inp").innerHTML = ""; 
-        for (const links of response) {
-            if (links.endsWith(".webp")) {
-                let a = links.split("+");
-                document.querySelector(".products").innerHTML += `
+            console.log(e.target.value, response);
+            document.querySelector(".inp").innerHTML = "";
+            for (const links of response) {
+                if (links.endsWith(".webp")) {
+                    let a = links.split("+");
+                    document.querySelector(".products").innerHTML += `
                     <div class="sho">
                         <div class="img">
                             <div class="icon">
@@ -120,11 +126,11 @@ document.querySelector(".inp").addEventListener("input",(e)=>{
                         e.addEventListener("click", () => {
                             document.querySelector(".ab").innerHTML++;
                             document.querySelector(".relative > span").style.color = "red"
-        
+
                             document.querySelector(".alert").classList.add("animate");
                             setTimeout(() => {
                                 document.querySelector(".alert").classList.remove("animate");
-        
+
                             }, 2000);
                             document.querySelector(".cart").innerHTML += `
                               <div class="item">
@@ -143,41 +149,41 @@ document.querySelector(".inp").addEventListener("input",(e)=>{
                             document.querySelectorAll(".item").forEach(a => {
                                 a.querySelector(".plus").addEventListener("click", () => {
                                     a.querySelector(".val").innerHTML++;
-                                    a.querySelector(".Prices").innerHTML = a.querySelector(".val").innerHTML*e.querySelector(".Price").innerHTML.split("$")[0]+" $";
-        
+                                    a.querySelector(".Prices").innerHTML = a.querySelector(".val").innerHTML * e.querySelector(".Price").innerHTML.split("$")[0] + " $";
+
                                 })
-        
+
                                 a.querySelector(".min").addEventListener("click", () => {
-                                    
-                                    if (a.querySelector(".val").innerHTML <1) {
+
+                                    if (a.querySelector(".val").innerHTML < 1) {
                                         console.log(a);
                                         a.querySelector(".min").classList.add("disabled");
                                         if (confirm("Do You Want to Delete Item !")) {
                                             a.remove();
                                             document.querySelector(".ab").innerHTML--;
-                                            
+
                                         }
                                         return;
                                     }
                                     a.querySelector(".val").innerHTML--;
-                                    a.querySelector(".Prices").innerHTML = a.querySelector(".val").innerHTML*e.querySelector(".Price").innerHTML.split("$")[0]+" $";
+                                    a.querySelector(".Prices").innerHTML = a.querySelector(".val").innerHTML * e.querySelector(".Price").innerHTML.split("$")[0] + " $";
                                 })
-        
+
                             })
                             document.querySelector(".cart span").addEventListener("click", () => {
                                 document.querySelector(".cart").style.display = "none";
                             })
                         })
-        
-        
+
+
                     })
+                }
             }
-        }
-    })
-    .catch(()=>{
-    document.querySelector(".products").innerHTML=`<img src="public/assets/other/Not-found.png" ></img>`
-        console.clear();
-    })
-      
+        })
+        .catch(() => {
+            document.querySelector(".products").innerHTML = `<img src="public/assets/other/Not-found.png" ></img>`
+            console.clear();
+        })
+
 
 })
