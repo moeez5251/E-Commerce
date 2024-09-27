0962ea507afef0bbc3f49bdeb75839bdf4598af1
moeez5251 <moeez66656@gmail.com>
2024-09-27 19:28:36 +0500

    Trying to make server live

--- netlify/functions/temp.js
+++ netlify/functions/temp.js
@@ -1,32 +1,3 @@
-const fs = require("fs");
-const path = require("path");
-
-exports.handler = async (event) => {
-  try {
-    const { slug } = event.queryStringParameters || {}; 
-    const imagesDir = path.join(process.cwd(), `public/assets/images/${slug}`);
-
-    if (!slug) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ error: "Slug is required" }),
-      };
-    }
-
-    if (!fs.existsSync(imagesDir)) {
-      throw new Error(`Directory not found: ${imagesDir}`);
-    }
-
-    const images = fs.readdirSync(imagesDir);
-    return {
-      statusCode: 200,
-      body: JSON.stringify(images),
-    };
-  } catch (error) {
-    console.error("Error:", error.message);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: error.message }),
-    };
-  }
+export default async (req, context) => {
+  return new Response("Hello, world!");
 };
